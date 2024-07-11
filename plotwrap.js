3031639747c3bc325ad5f98ef6fc8a85fd665fc8
hanlev <hanlev@gmail.com>
2024-07-10 22:42:27 -0500

    Added Gaussian lineshape option

--- plotwrap.js
+++ plotwrap.js
@@ -1,4 +1,4 @@
-function plotspectrum(plocs,pamps,xmin,xmax,xlab,ylab,gtitle,fwhh,revplot,invplot) {
+function plotspectrum(plocs,pamps,xmin,xmax,xlab,ylab,gtitle,fwhh,revplot,invplot,lintyp) {
 
     // Key to input variables:
 	// plocs = array containing peak locations (e.g. vibrational frequencies)
@@ -11,27 +11,44 @@ function plotspectrum(plocs,pamps,xmin,xmax,xlab,ylab,gtitle,fwhh,revplot,invplo
 	// revplot = 'reversed' to reverse x-axis or 'false' to leave x-axis alone
 	// invplot = 1 or -1 (1 to keep peaks with positive values, -1 to 
 	//           give peaks negative values, as for an IR spectrum, to match expt.
+	// lintyp = "lorent" (Lorentzian) or "gauss" (Gaussian) to indicate 
+	//          the lineshape function for the spectrum
 	
     var w = xmin;
     var j;
     var amp = [];    // amplitudes on y-axis of spectrum
     var lambda = [];  // frequencies on x-axis of spectrum
+    var sig = fwhh/(2*Math.sqrt(2*Math.log(2))); // sigma in Gaussian lineshape
+    var ltstring;
 
-    // Lorentzian form for peaks
-
-    while (w <= xmax) {
-        var ph = 0;
-        for (j = 0; j < plocs.length; j++) {
-            ph = invplot*pamps[j]*(1/3.141592654)*(0.5*fwhh)/((w-plocs[j])**2 + (0.5*fwhh)**2) + ph;
+    if (lintyp == "lorent") {
+	ltstring = "Lorentzian";
+        while (w <= xmax) {
+            var ph = 0;
+            for (j = 0; j < plocs.length; j++) {
+                ph = invplot*pamps[j]*(1/3.141592654)*(0.5*fwhh)/((w-plocs[j])**2 + (0.5*fwhh)**2) + ph;
+            }
+            lambda.push(w);
+            amp.push(ph);
+            w++;
         }
-        lambda.push(w);
-        amp.push(ph);
-        w++;
+    }
+    else if (lintyp == "gauss") {
+	ltstring = "Gaussian";
+        while (w <= xmax) {
+            var ph = 0;
+            for (j = 0; j < plocs.length; j++) {
+                ph = invplot*pamps[j]*(1/(sig*Math.sqrt(2*3.141592654)))*Math.exp((-((w-plocs[j])**2))/(2*(sig**2))) + ph;
+            }
+            lambda.push(w);
+            amp.push(ph);
+            w++;
+        }
     }
 
     // Set up the plotly graphing information and create the graph
 
-    var gtitleplus = gtitle + " with FWHH = " + fwhh;
+    var gtitleplus = gtitle + "; FWHH = " + fwhh + "; Lineshape = " + ltstring;
 
     var trace = {
         x: lambda,
