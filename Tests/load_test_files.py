8d198d0b87ad36dda8dafd265500021f0edf4dd1
Tim Leverentz <timlev@gmail.com>
2024-07-12 16:46:29 -0500

    Linted python code

--- Tests/load_test_files.py
+++ Tests/load_test_files.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time, os
+#from selenium.webdriver.common.keys import Keys
+import time
+import os
 from pathlib import Path
 
 
